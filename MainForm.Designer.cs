cd020d8356e3b094cde327c77358ef37ac2c323b
LenaBrokaw <146871302+LenaBrokaw@users.noreply.github.com>
2025-02-17 14:02:02 -0500

    added files

---
+++ MainForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace _42Assignment1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDarkMode = new System.Windows.Forms.Button();
+            this.btnLightMode = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pbxSquare22 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare21 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare20 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare12 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare11 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare10 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare02 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare01 = new System.Windows.Forms.PictureBox();
+            this.pbxSquare00 = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare00)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDarkMode
+            // 
+            this.btnDarkMode.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnDarkMode.Font = new System.Drawing.Font("Sitka Display Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDarkMode.ForeColor = System.Drawing.Color.White;
+            this.btnDarkMode.Location = new System.Drawing.Point(586, 258);
+            this.btnDarkMode.Name = "btnDarkMode";
+            this.btnDarkMode.Size = new System.Drawing.Size(136, 46);
+            this.btnDarkMode.TabIndex = 30;
+            this.btnDarkMode.Text = "Dark Mode";
+            this.btnDarkMode.UseVisualStyleBackColor = false;
+            this.btnDarkMode.Click += new System.EventHandler(this.btnDarkMode_Click);
+            // 
+            // btnLightMode
+            // 
+            this.btnLightMode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnLightMode.Font = new System.Drawing.Font("Sitka Display Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLightMode.Location = new System.Drawing.Point(586, 180);
+            this.btnLightMode.Name = "btnLightMode";
+            this.btnLightMode.Size = new System.Drawing.Size(136, 46);
+            this.btnLightMode.TabIndex = 29;
+            this.btnLightMode.Text = "Light Mode";
+            this.btnLightMode.UseVisualStyleBackColor = false;
+            this.btnLightMode.Click += new System.EventHandler(this.btnLightMode_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblMessage.Location = new System.Drawing.Point(170, 95);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(357, 30);
+            this.lblMessage.TabIndex = 28;
+            this.lblMessage.Text = "Welcome to the Tic-Tac-Toe game!";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(234, 808);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(230, 58);
+            this.btnExit.TabIndex = 27;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pbxSquare22
+            // 
+            this.pbxSquare22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare22.Location = new System.Drawing.Point(382, 591);
+            this.pbxSquare22.Name = "pbxSquare22";
+            this.pbxSquare22.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare22.TabIndex = 26;
+            this.pbxSquare22.TabStop = false;
+            this.pbxSquare22.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare21
+            // 
+            this.pbxSquare21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare21.Location = new System.Drawing.Point(230, 591);
+            this.pbxSquare21.Name = "pbxSquare21";
+            this.pbxSquare21.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare21.TabIndex = 25;
+            this.pbxSquare21.TabStop = false;
+            this.pbxSquare21.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare20
+            // 
+            this.pbxSquare20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare20.Location = new System.Drawing.Point(76, 591);
+            this.pbxSquare20.Name = "pbxSquare20";
+            this.pbxSquare20.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare20.TabIndex = 24;
+            this.pbxSquare20.TabStop = false;
+            this.pbxSquare20.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare12
+            // 
+            this.pbxSquare12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare12.Location = new System.Drawing.Point(382, 385);
+            this.pbxSquare12.Name = "pbxSquare12";
+            this.pbxSquare12.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare12.TabIndex = 23;
+            this.pbxSquare12.TabStop = false;
+            this.pbxSquare12.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare11
+            // 
+            this.pbxSquare11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare11.Location = new System.Drawing.Point(230, 385);
+            this.pbxSquare11.Name = "pbxSquare11";
+            this.pbxSquare11.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare11.TabIndex = 22;
+            this.pbxSquare11.TabStop = false;
+            this.pbxSquare11.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare10
+            // 
+            this.pbxSquare10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare10.Location = new System.Drawing.Point(76, 385);
+            this.pbxSquare10.Name = "pbxSquare10";
+            this.pbxSquare10.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare10.TabIndex = 21;
+            this.pbxSquare10.TabStop = false;
+            this.pbxSquare10.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare02
+            // 
+            this.pbxSquare02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare02.Location = new System.Drawing.Point(382, 180);
+            this.pbxSquare02.Name = "pbxSquare02";
+            this.pbxSquare02.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare02.TabIndex = 20;
+            this.pbxSquare02.TabStop = false;
+            this.pbxSquare02.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare01
+            // 
+            this.pbxSquare01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare01.Location = new System.Drawing.Point(230, 180);
+            this.pbxSquare01.Name = "pbxSquare01";
+            this.pbxSquare01.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare01.TabIndex = 19;
+            this.pbxSquare01.TabStop = false;
+            this.pbxSquare01.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // pbxSquare00
+            // 
+            this.pbxSquare00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxSquare00.Location = new System.Drawing.Point(76, 180);
+            this.pbxSquare00.Name = "pbxSquare00";
+            this.pbxSquare00.Size = new System.Drawing.Size(148, 199);
+            this.pbxSquare00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxSquare00.TabIndex = 18;
+            this.pbxSquare00.TabStop = false;
+            this.pbxSquare00.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Sitka Banner Semibold", 25F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(219, 17);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(259, 72);
+            this.lblTitle.TabIndex = 17;
+            this.lblTitle.Text = "Tic-Tac-Toe";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 891);
+            this.Controls.Add(this.btnDarkMode);
+            this.Controls.Add(this.btnLightMode);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.pbxSquare22);
+            this.Controls.Add(this.pbxSquare21);
+            this.Controls.Add(this.pbxSquare20);
+            this.Controls.Add(this.pbxSquare12);
+            this.Controls.Add(this.pbxSquare11);
+            this.Controls.Add(this.pbxSquare10);
+            this.Controls.Add(this.pbxSquare02);
+            this.Controls.Add(this.pbxSquare01);
+            this.Controls.Add(this.pbxSquare00);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "MainForm";
+            this.Text = "Tic-Tac-Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSquare00)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnDarkMode;
+        private System.Windows.Forms.Button btnLightMode;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pbxSquare22;
+        private System.Windows.Forms.PictureBox pbxSquare21;
+        private System.Windows.Forms.PictureBox pbxSquare20;
+        private System.Windows.Forms.PictureBox pbxSquare12;
+        private System.Windows.Forms.PictureBox pbxSquare11;
+        private System.Windows.Forms.PictureBox pbxSquare10;
+        private System.Windows.Forms.PictureBox pbxSquare02;
+        private System.Windows.Forms.PictureBox pbxSquare01;
+        private System.Windows.Forms.PictureBox pbxSquare00;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,272 @@
+﻿/*
+ * MainForm.cs
+ * Assignment 1
+ * Section 2
+ * Group 42 (Lena Brokaw, Ana Isakov)
+*/
+
+// Imports
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _42Assignment1
+{
+    public partial class MainForm : Form
+    {
+        // Global variables
+        public bool win = false;
+        const string X_WIN = "xxx"; // pattern needed for x to win
+        const string O_WIN = "ooo"; // pattern needed for o to win
+        public int player = 0; // variable to control whose turn it is (0 is O, 1 is X)
+        public char[] output = { '.', '.', '.' }; // array to hold row, column, symbol of picturebox square
+        // array to hold contents of 3 by 3 tic-tac-toe grid
+        public char[,] grid = {
+            {'0','0','0'},
+            {'0','0','0'},
+            {'0','0','0'}
+        };
+
+        /// <summary>
+        /// Method to check for a horizontal win (loops through grid by its rows).
+        /// </summary>
+        public void RowCheck()
+        {
+            string horizontal = "";
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    horizontal += grid[i, j]; // loops by row
+                }
+                // check against x pattern and o pattern
+                if (horizontal == X_WIN || horizontal== O_WIN)
+                {
+                    win = true; // set win to true
+                }
+                horizontal = ""; // reset horizontal check to empty after each row
+            }
+        }
+        /// <summary>
+        /// Method to check for a vertical win (loops through grid by its columns).
+        /// </summary>
+        public void ColumnCheck() {
+            string vertical = "";
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    vertical += grid[j, i]; // loops by column
+                }
+                // check against x pattern and o pattern
+                if (vertical == X_WIN || vertical == O_WIN)
+                { 
+                    win = true; // set win to true
+                }
+                vertical = ""; // reset vertical check to empty after each column
+            }
+        }
+        /// <summary>
+        /// Method to check for a diagonal win (loops through only one dimension of the grid).
+        /// </summary>
+        public void DiagonalCheck() {
+            string diagonal1 = ""; // diagonal starting from top left
+            string diagonal2 = ""; // diagonal starting from top right
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                diagonal1 += grid[i, i]; // checks [0, 0], [1, 1], [2, 2]
+                if (diagonal1 == X_WIN || diagonal1 == O_WIN)
+                {
+                    win = true;         
+                    break;
+                }
+                diagonal2 += grid[i, grid.GetLength(1) - 1 - i]; // checks [0, 2], [1, 1], [2, 0]
+                if (diagonal2 == X_WIN || diagonal2 == O_WIN)
+                {
+                    win = true;
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to change whose turn it is (player X or O).
+        /// </summary>
+        public void PlayerChange()
+        {
+            if (player == 0) { 
+                player = 1; 
+            }
+            else if (player == 1) {
+                player = 0;
+            }
+        }
+
+        /// <summary>
+        /// Method that allows the player to complete a turn by extracting the row and column 
+        /// numbers of the move, along with the player's symbol, and adding it to the 3 by 3 grid
+        /// </summary>
+        /// <param name="output"></param>
+        public void CellInput(char[] output)
+        {
+            int row = int.Parse(output[0].ToString());
+            int col = int.Parse(output[1].ToString());
+            char symbol = output[2];
+            grid[row, col] = symbol; // adds the player's symbol to the grid
+        }
+
+        /// <summary>
+        /// Clears and resets all controls and variables related to the game. User can then restart.
+        /// </summary>
+        public void ClearAll()
+        {
+            Task.Delay(2500).Wait(); // wait for a few seconds (2.5) before resetting the game so the
+                                     // user has time to read the output message
+            // loop through all pictureboxes
+            foreach (var square in Controls.OfType<PictureBox>())
+            {
+                square.Image = null; // set each image to null ("none")
+                square.Enabled = true; // enable all pictureboxes
+            }
+            // reset variables
+            win = false;
+            player = 0;
+
+            lblMessage.Text = "Game Reset"; // let user know that we've reset
+
+            GridReset(); // reset grid
+        }
+
+        /// <summary>
+        /// Method to reset the grid array to empty
+        /// </summary>
+        public void GridReset()
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    grid[i, j] = '0'; // sets each element to 0, rather than O or X
+                }
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for all pictureboxes
+        private void Square_Click(object sender, EventArgs e)
+        {
+            //extracts the picturebox name and co-ordinates from the sender
+            PictureBox picBox = (PictureBox)sender;
+            string picBoxName = picBox.Name;
+            char x_coord = picBoxName[picBoxName.Length-1]; 
+            char y_coord = picBoxName[picBoxName.Length-2];
+
+            //Sets the image of the picturebox that was clicked, dependant on the player
+            if (player == 0) { picBox.Image = _42Assignment1.Properties.Resources.letter_o; }
+            else { picBox.Image = _42Assignment1.Properties.Resources.letter_x; }
+
+            //disables the control, ensuring the square cannot be stolen by the other player
+            picBox.Enabled = false;
+
+            //sends the coordinates of the square and the player symbol to an array
+            output[0] = y_coord;
+            output[1] = x_coord;
+            if (player == 0)
+            { 
+                output[2] = 'o';
+            }
+            else 
+            { 
+                output[2] = 'x';
+            }
+
+            //adds x or o to the array at the coordinate of the clicked square
+            CellInput(output);
+
+            // -- Check Logic --- 
+            RowCheck(); // horizontal win
+            ColumnCheck(); // vertical win
+            DiagonalCheck(); // diagonal win
+
+            // loop to check if all squares on the grid are full
+            bool isFull = true;
+            foreach (var square in Controls.OfType<PictureBox>())
+            {
+                if (square.Image == null) // if any of them don't have a picture
+                { 
+                    isFull = false; // the grid is not full
+                }
+            }
+
+            // --- GAME END LOGIC ---
+
+            if (win) // check for a win
+            { 
+                // Code to get player name
+                string playerName;
+                if (player == 0) 
+                { 
+                    playerName = "O";
+                }
+                else 
+                { 
+                    playerName = "X";
+                }
+
+                // output winner!
+                lblMessage.Text = $"Player {playerName} wins!";
+                
+                ClearAll(); // reset the game
+                return; // exit event handler
+            }
+            else if (isFull) // check if all spaces are full (tie)
+            {
+                // if no one has won, and all spaces are full, there is a tie
+                // output tie status
+                lblMessage.Text = "It's a tie!";
+                ClearAll(); // reset the game
+                return; // exit event handler
+            }
+
+            // if neither of these conditions were satisfied, keep playing
+            
+            // changes the player, 0 to 1 and vice versa
+            PlayerChange();
+        }
+
+        // Changes form to light mode
+        private void btnLightMode_Click(object sender, EventArgs e)
+        {
+            MainForm.ActiveForm.BackColor = Color.White;
+            lblTitle.ForeColor = Color.Black;
+            lblMessage.ForeColor = Color.DarkBlue;
+        }
+        // Changes form to dark mode
+        private void btnDarkMode_Click(object sender, EventArgs e)
+        {
+            MainForm.ActiveForm.BackColor = Color.Black;
+            lblTitle.ForeColor = Color.White;
+            lblMessage.ForeColor = Color.FromArgb(128, 255, 255);
+        }
+        // Exit the form
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
